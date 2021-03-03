6a8f37edabf05d0c135f0edc43cede887d05371b
HXHGTS <38959826+HXHGTS@users.noreply.github.com>
2021-03-04 05:00:44 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,4 +6,18 @@ yum install dnsmasq -y
 
 echo 正在配置dnsmasq. . .
 
+curl https://cdn.jsdelivr.net/gh/HXHGTS/dnsmasq-install/dnsmasq.conf > /etc/dnsmasq.conf
+
+echo 正在启动dnsmasq. . .
+
+systemctl restart dnsmasq
+
+echo --------------------------------
+
+echo tcp/udp端口:53
+
+echo --------------------------------
+
+echo 成功安装dnsmasq!
+
 exit 0
